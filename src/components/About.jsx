5254b1ea28abc63391085eabf52c3819b1c5d949
haripathakchhetri <pathakhari570@gmail.com>
2024-10-18 22:46:47 +0545

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,23 @@
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full ">
+
+        <div className="pb-8 ">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+        </div>
+
+        <p className=" text-sm md:text-xl  mt-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia voluptas aperiam nemo consequuntur quo eligendi consequatur harum voluptates ut, architecto molestias? Odit earum minus cum accusantium qui beatae ipsum, itaque culpa quaerat, totam atque, impedit eos! Quod expedita ipsam numquam, reiciendis soluta, sint obcaecati magnam dolore assumenda repellat, quam rem.</p>
+
+        <br />
+
+        <p className=" text-sm md:text-xl mt-5 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum magnam tempore facilis harum assumenda ipsam neque accusantium est provident similique repellat nisi consectetur asperiores maiores hic quidem velit dolorem, explicabo quod expedita ipsum minus officia corporis saepe. Amet rem, iure incidunt est necessitatibus reprehenderit aliquam quam quaerat vitae quas molestiae.</p>
+
+      </div>
+
+
+    </div>
+  )
+}
+export default About
